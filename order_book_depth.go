120ad6b1a7db928a0aade8a672a3fc0a664b88d3
nrabe1010 <nrabe1010@gmail.com>
2023-02-01 14:51:45 -0500

    add Ndepth

--- order_book_depth.go
+++ order_book_depth.go
@@ -23,3 +23,37 @@ func (ob *OrderBook) Depth() *Depth {
 
 	return &Depth{bids, asks}
 }
+
+// NDepth retruns the depth from best bid/ask back nAsks and nBids.
+func (ob *OrderBook) NDepth(nAsks int, nBids int) *Depth {
+	defer ob.RUnlock()
+	ob.RLock()
+
+	asks := make([]*PriceLevel, 0)
+	level := ob.asks.MinPriceQueue()
+	i := 1
+	for level != nil {
+		asks = append(asks, NewPriceLevel(level.price, level.amount))
+		level = ob.asks.GreaterThan(level.price)
+
+		i++
+		if i > nAsks {
+			level = nil
+		}
+	}
+
+	bids := make([]*PriceLevel, 0)
+	level = ob.bids.MaxPriceQueue()
+	i = 1
+	for level != nil {
+		bids = append(bids, NewPriceLevel(level.price, level.amount))
+		level = ob.bids.LessThan(level.price)
+
+		i++
+		if i > nBids {
+			level = nil
+		}
+	}
+
+	return &Depth{bids, asks}
+}
